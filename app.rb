e5a16c1602c237d8bf2a1e9a5c7350fc9cc87ce2
M01-M01 <marlonsaddress@gmail.com>
2018-07-16 17:13:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  get '/' do
+    response.status = 200
+    response.write "Hello, World!"
+
+  end
+
 
 
 end
